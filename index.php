b2299fbc2c221e4f8919d03aa9fed29727874705
stephbak <stephane.bakum@sfr.fr>
2018-12-14 16:03:28 +0100

    exercice5

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>php partie1</title>
+</head>
+<body>
+  <p>
+    <?php
+    $int = NULL;
+    echo $int;
+    ?>
+  </p>
+  <p>
+    <?php
+    $int = 10;
+    echo $int;
+    ?>
+  </p>
+</body>
+</html>
